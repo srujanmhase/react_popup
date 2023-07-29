ce033f6ff26e3ebf80719e4520ea5877696224c7
Srujan <srujanmhase5@gmail.com>
2023-07-30 01:37:57 +0530

    adding exports

--- lib/react_popup.dart
+++ lib/react_popup.dart
@@ -1,7 +1,6 @@
 library react_popup;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'view/animated_overlay.dart';
+export 'view/reaction_element.dart';
+export 'view/reaction_parent.dart';
+export 'view/reactions_wrapper.dart';
