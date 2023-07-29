a2b2a00a1a96c5daa376a876a11225bc9f402c16
Srujan <srujanmhase5@gmail.com>
2023-07-30 01:49:26 +0530

    depend on flutter_bloc

--- lib/bloc/reaction_parent_bloc.dart
+++ lib/bloc/reaction_parent_bloc.dart
@@ -1,5 +1,5 @@
-import 'package:bloc/bloc.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'reaction_parent_event.dart';
 part 'reaction_parent_state.dart';
